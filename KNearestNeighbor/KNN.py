7a53d043792935e1a6bcb7891856981f2f005382
imabing <53820071@qq.com>
2017-01-01 03:24:02 +0800

    k

--- KNearestNeighbor/KNN.py
+++ KNearestNeighbor/KNN.py
@@ -1,5 +1,6 @@
 # -*- coding: cp936 -*-
 import numpy as np
+import matplotlib.pyplot as plt
 import operator
 def createDataSet():
     data=np.array([[1,1],[1,2],[10,10],[10,11]])
@@ -20,5 +21,7 @@ def knn(point,data,labels,k):
     sortedSelectK = sorted(selectK.iteritems(),key=operator.itemgetter(1),reverse=True)
     return sortedSelectK[0][0]
 if __name__ == '__main__':
-    data,lables=createDataSet()
-    print knn([100,100],data,lables,3)
+    data,labels=createDataSet()
+    plt.plot(data, labels)
+    plt.show()
+    print knn([5,7],data,labels,3)
