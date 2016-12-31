043918db0fd405b7e7d3ce603b84d42dea1dc940
imabing <53820071@qq.com>
2017-01-01 03:04:42 +0800

    k

--- KNearestNeighbor/KNN.py
+++ KNearestNeighbor/KNN.py
@@ -1,14 +1,24 @@
+# -*- coding: cp936 -*-
 import numpy as np
 import operator
 def createDataSet():
     data=np.array([[1,1],[1,2],[10,10],[10,11]])
-    labels=['A','A','B',b'']
+    labels=['A','A','B','B']
     return data,labels
 def knn(point,data,labels,k):
-    # 鏁版嵁闆嗘牱鏈暟
     dataSize=data.shape[0]
-
-    return dataSize
+    subMatrix=np.tile(point,(dataSize,1))-data
+    matrixSq=subMatrix**2
+    distance=(matrixSq.sum(axis=1))**0.5
+    # 升序排列样本索引如： [2 3 1 0]
+    sortDistanceIndex=distance.argsort()
+    # 取距离最短的k个点
+    selectK={}
+    for i in range(k):
+        kLabel = labels[sortDistanceIndex[i]]
+        selectK[kLabel] = selectK.get(kLabel,0) + 1
+    sortedSelectK = sorted(selectK.iteritems(),key=operator.itemgetter(1),reverse=True)
+    return sortedSelectK[0][0]
 if __name__ == '__main__':
     data,lables=createDataSet()
-    print knn([1,3],data,lables,4)
+    print knn([100,100],data,lables,3)
