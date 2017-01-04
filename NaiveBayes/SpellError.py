0e117c238515c872bfd18569f1746d8e0fa562a1
imabing <53820071@qq.com>
2017-01-05 03:03:38 +0800

    emailcheck

--- NaiveBayes/SpellError.py
+++ NaiveBayes/SpellError.py
@@ -1,42 +1,31 @@
-import re
+# -*- coding:utf-8 -*-
+import re   #正则语言模块（re）和collections模块
 from collections import Counter
-
+# 1.定义words()函数，用来取出文本库的每一个词。
 def words(text): return re.findall(r'\w+', text.lower())
-
-WORDS = Counter(words(open('big.txt').read()))
-
-def P(word, N=sum(WORDS.values())):
-    "Probability of `word`."
-    return WORDS[word] / N
-
+#2.统计词语在文本库的出现频率键值结构
+Dictionariy = Counter(words(open('big.txt').read()))
+#3.统计一个词在词典中的概率
+def P(word, N=sum(Dictionariy.values())):
+    return Dictionariy[word] / N
 def correction(word):
-    "Most probable spelling correction for word."
     return max(candidates(word), key=P)
-
 def candidates(word):
-    "Generate possible spelling corrections for word."
     return (known([word]) or known(edits1(word)) or known(edits2(word)) or [word])
-
 def known(words):
-    "The subset of `words` that appear in the dictionary of WORDS."
-    return set(w for w in words if w in WORDS)
-
+    return set(w for w in words if w in Dictionariy)
 def edits1(word):
-    "All edits that are one edit away from `word`."
     letters    = 'abcdefghijklmnopqrstuvwxyz'
-    splits     = [(word[:i], word[i:])    for i in range(len(word) + 1)]
-    deletes    = [L + R[1:]               for L, R in splits if R]
-    transposes = [L + R[1] + R[0] + R[2:] for L, R in splits if len(R)>1]
-    replaces   = [L + c + R[1:]           for L, R in splits if R for c in letters]
-    inserts    = [L + c + R               for L, R in splits for c in letters]
+    splits     = [(word[:i], word[i:])    for i in range(len(word) + 1)]    #'abc'=>[('', 'abc'), ('a', 'bc'), ('ab', 'c'), ('abc', '')]
+    deletes    = [L + R[1:]               for L, R in splits if R]          #'abc'=>['bc', 'ac', 'ab'] 。
+    transposes = [L + R[1] + R[0] + R[2:] for L, R in splits if len(R)>1]   #'abc' => ['bac', 'acb']
+    replaces   = [L + c + R[1:]           for L, R in splits if R for c in letters] #'abc'=> ['abc', 'bbc', 'cbc', ... , 'abx', ' aby', 'abz' ]  26*3个
+    inserts    = [L + c + R               for L, R in splits for c in letters] #'abc' =>['aabc', 'babc', 'cabc', ..., 'abcx', 'abcy', 'abcz']，一共包含104个词（26 × 4）。
     return set(deletes + transposes + replaces + inserts)
-
 def edits2(word):
-    "All edits that are two edits away from `word`."
     return (e2 for e1 in edits1(word) for e2 in edits1(e1))
-
-
-
 if __name__ == '__main__':
-    word = 'banana'
-    print correction(word)
+    word = 'deskw'
+    # print correction(word)
+    # print sum(Dictionariy.values())
+    print edits1(word)
