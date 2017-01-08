321fa9e0d3bcdf7462d18e67a8aa9885837f8fb7
imabing <53820071@qq.com>
2017-01-09 00:09:33 +0800

    bayes

--- NaiveBayes/Bayes.py
+++ NaiveBayes/Bayes.py
@@ -15,29 +15,28 @@ def loadDataSet():
 def createVocabList(dataSet):
     vocabSet = set([])
     for document in dataSet:
-        print  document
         vocabSet = vocabSet | set(document)   #并集
     return list(vocabSet)
-
-def setOfWords2Vec(vocabList, inputSet):
-    returnVec = [0] * len(vocabList)
+# 3、把输入文本转换为一个与语料去重库同大小的行向量，并统计对应的词语个数
+def setOfWords2Vec(vocabSet, inputSet):
+    returnVec = [0] * len(vocabSet)
     for word in inputSet:
-        if word in vocabList:
-            returnVec[vocabList.index(word)] = 1
+        if word in vocabSet:
+            returnVec[vocabSet.index(word)] = 1
         else:
             print "the word: %s is not in my Vocabulary!" % word
     return returnVec
 
 
-def trainNB0(trainMatrix, trainCategory):
-    numTrainDocs = len(trainMatrix)
+def trainNB0(trainMatrix, trainCategory):  #输入：语料库向量矩阵与标签
+    numTrain = len(trainMatrix)
     numWords = len(trainMatrix[0])
-    pAbusive = sum(trainCategory) / float(numTrainDocs)
-    p0Num = np.ones(numWords);
+    pAbusive = sum(trainCategory) / float(numTrain)   #去重后关键字与总关键字之比
+    p0Num = np.ones(numWords);   #构造初始值为1的矩阵，numpy.zeros初始值为0
     p1Num = np.ones(numWords)  # change to ones()
     p0Denom = 2.0;
     p1Denom = 2.0  # change to 2.0
-    for i in range(numTrainDocs):
+    for i in range(numTrain):
         if trainCategory[i] == 1:
             p1Num += trainMatrix[i]
             p1Denom += sum(trainMatrix[i])
@@ -59,22 +58,23 @@ def classifyNB(vec2Classify, p0Vec, p1Vec, pClass1):
 
 
 def testingNB():
-    listOPosts, listClasses = loadDataSet()
-    myVocabList = createVocabList(listOPosts)
-    trainMat = []
-    for postinDoc in listOPosts:
-        trainMat.append(setOfWords2Vec(myVocabList, postinDoc))
-    p0V, p1V, pAb = trainNB0(np.array(trainMat), np.array(listClasses))
-    testEntry = ['love', 'my', 'dalmation']
-    thisDoc = np.array(setOfWords2Vec(myVocabList, testEntry))
+    dataSet, labels = loadDataSet()            #原始语料
+    vocabList = createVocabList(dataSet)       #关键字去重
+    trainMat = []                              #把语料的每一个样本都转换为向量矩阵[[0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1], [0, 0,...
+    for line in dataSet:
+        trainMat.append(setOfWords2Vec(vocabList, line))
+    p0V, p1V, pAb = trainNB0(np.array(trainMat), np.array(labels))
+    print trainMat
+    print np.array(trainMat)
+    testEntry = ['love', 'my', 'dalmation','哈哈哈']
+    thisDoc = np.array(setOfWords2Vec(vocabList, testEntry))
     print testEntry, 'classified as: ', classifyNB(thisDoc, p0V, p1V, pAb)
     testEntry = ['stupid', 'garbage']
-    thisDoc = np.array(setOfWords2Vec(myVocabList, testEntry))
+    thisDoc = np.array(setOfWords2Vec(vocabList, testEntry))
     print testEntry, 'classified as: ', classifyNB(thisDoc, p0V, p1V, pAb)
 
 
 if __name__ == '__main__':
-    # testingNB()
+    testingNB()
     dataset, labels=loadDataSet()
-    print dataset
-    print createVocabList(dataset)
+    vocabSet=createVocabList(dataset)
