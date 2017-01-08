68cfd100d7aa1d9e79537894c43555faea030f59
imabing <53820071@qq.com>
2017-01-08 22:23:26 +0800

    byes

---
+++ NaiveBayes/Bayes.py
@@ -0,0 +1,80 @@
+# -*- coding:utf-8 -*-
+import numpy as np
+def loadDataSet():
+    # 1、语料库
+    dataSet = [['my', 'dog', 'has', 'flea', 'problems', 'help', 'please'],
+               ['maybe', 'not', 'take', 'him', 'to', 'dog', 'park', 'stupid'],
+               ['my', 'dalmation', 'is', 'so', 'cute', 'I', 'love', 'him'],
+               ['stop', 'posting', 'stupid', 'worthless', 'garbage'],
+               ['mr', 'licks', 'ate', 'my', 'steak', 'how', 'to', 'stop', 'him'],
+               ['quit', 'buying', 'worthless', 'dog', 'food', 'stupid']]
+    # 类别
+    labels = [0, 1, 0, 1, 0, 1]
+    return dataSet, labels
+    # 2、语料去重
+def createVocabList(dataSet):
+    vocabSet = set([])
+    for document in dataSet:
+        print  document
+        vocabSet = vocabSet | set(document)   #并集
+    return list(vocabSet)
+
+def setOfWords2Vec(vocabList, inputSet):
+    returnVec = [0] * len(vocabList)
+    for word in inputSet:
+        if word in vocabList:
+            returnVec[vocabList.index(word)] = 1
+        else:
+            print "the word: %s is not in my Vocabulary!" % word
+    return returnVec
+
+
+def trainNB0(trainMatrix, trainCategory):
+    numTrainDocs = len(trainMatrix)
+    numWords = len(trainMatrix[0])
+    pAbusive = sum(trainCategory) / float(numTrainDocs)
+    p0Num = np.ones(numWords);
+    p1Num = np.ones(numWords)  # change to ones()
+    p0Denom = 2.0;
+    p1Denom = 2.0  # change to 2.0
+    for i in range(numTrainDocs):
+        if trainCategory[i] == 1:
+            p1Num += trainMatrix[i]
+            p1Denom += sum(trainMatrix[i])
+        else:
+            p0Num += trainMatrix[i]
+            p0Denom += sum(trainMatrix[i])
+    p1Vect = np.log(p1Num / p1Denom)  # change to log()
+    p0Vect = np.log(p0Num / p0Denom)  # change to log()
+    return p0Vect, p1Vect, pAbusive
+
+
+def classifyNB(vec2Classify, p0Vec, p1Vec, pClass1):
+    p1 = sum(vec2Classify * p1Vec) + np.log(pClass1)  # element-wise mult
+    p0 = sum(vec2Classify * p0Vec) + np.log(1.0 - pClass1)
+    if p1 > p0:
+        return 1
+    else:
+        return 0
+
+
+def testingNB():
+    listOPosts, listClasses = loadDataSet()
+    myVocabList = createVocabList(listOPosts)
+    trainMat = []
+    for postinDoc in listOPosts:
+        trainMat.append(setOfWords2Vec(myVocabList, postinDoc))
+    p0V, p1V, pAb = trainNB0(np.array(trainMat), np.array(listClasses))
+    testEntry = ['love', 'my', 'dalmation']
+    thisDoc = np.array(setOfWords2Vec(myVocabList, testEntry))
+    print testEntry, 'classified as: ', classifyNB(thisDoc, p0V, p1V, pAb)
+    testEntry = ['stupid', 'garbage']
+    thisDoc = np.array(setOfWords2Vec(myVocabList, testEntry))
+    print testEntry, 'classified as: ', classifyNB(thisDoc, p0V, p1V, pAb)
+
+
+if __name__ == '__main__':
+    # testingNB()
+    dataset, labels=loadDataSet()
+    print dataset
+    print createVocabList(dataset)
